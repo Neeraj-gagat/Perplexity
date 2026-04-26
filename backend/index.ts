6ff8272ea3a3abd6f1cab29cf8cbf90bdec5c703
Neeraj-gagat <neerajgagat9999@gmail.com>
2026-04-27 00:00:06 +0530

    initial plan

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express from "express";
+const app = express()
+
+app.get("/chat", async (req, res) => {
+    // query from the user
+
+    // make sure only user with paid plan are able to hit the endpoint 
+
+    // indexing websearch for similar query
+
+    // web search for resources
+
+    // do context enginnering on prompt + web search responses
+
+    // hit the LLM and stream back the response 
+
+    // also stream back the resources and the follow up questons (which we get from the parallel LLM call)
+})
+
+app.listen(3000);
