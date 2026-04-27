04f29d3132cd71f7c2a6920c5c9cbbb4cf939a89
Neeraj-gagat <neerajgagat9999@gmail.com>
2026-04-28 00:48:38 +0530

    chat endpoint kinda done

--- backend/index.ts
+++ backend/index.ts
@@ -1,20 +1,64 @@
 import express from "express";
+import { tavily } from "@tavily/core"
+import z, { url } from "zod"
+import { Output, streamText } from 'ai'
+import { PROMPT_TEMPLATE, SYSTEM_PROMPT } from "./prompt";
+
+const client = tavily({apiKey: process.env.TAVILY_API_KEY}) 
+
 const app = express()
+
+app.use(express.json());
 
 app.get("/chat", async (req, res) => {
     // query from the user
+    const query = req.body.query;
 
-    // make sure only user with paid plan are able to hit the endpoint 
+    // make sure only user with paid plan are able to hit the writepoint 
 
-    // indexing websearch for similar query
+    // checking indexed results for similar websearch query
 
     // web search for resources
+    const webSearchResponse = await client.search(query, {
+        searchDepth: "advanced"
+    });
+
+    const webSearchResult = webSearchResponse.results
 
     // do context enginnering on prompt + web search responses
+    const prompt = PROMPT_TEMPLATE
+        .replace("{{WEB_SEARCH_RESULTS}}", JSON.stringify(webSearchResult))
+        .replace("{{USER_QUERY}}", JSON.stringify(query));
 
     // hit the LLM and stream back the response 
+    const result = streamText({
+    model: 'openai/gpt-5.4',
+    prompt: prompt,
+    system:SYSTEM_PROMPT,
+    // output: Output.object({
+    // schema: z.object({
+    //   followUps: z.array(z.string()),
+    //     answer: z.string
+    //     }),
+    // }),
+    })
+
+    // res.header('Cache-Control', 'no-cache')
+    // res.header('Content-Type', 'text/event-stream')
+
+    for await (const textPart of result.textStream) {
+        res.write(textPart);
+    }
+
+    res.write("\n<SOURCES>\n")
 
     // also stream back the resources and the follow up questons (which we get from the parallel LLM call)
+    res.write(JSON.stringify(webSearchResult.map(result => ({url: result.url}))));
+
+    res.write("\n</SOURCES>\n")
+
+    // close the event stream
+    res.end()
 })
 
 app.listen(3000);
--- backend/prompt.ts
+++ backend/prompt.ts
@@ -1,13 +1,31 @@
 
-export const STSTEM_PROMPT = `
+export const SYSTEM_PROMPT = `
     you are an expert asistant called perplexity. Your job is simple, given the USER-QUERY and a bunch of web search response, try to ansure the user query to the best of your abilities. YOU DONT HAVE ACESS TO ANY TOOLS. you are being given all the context that is needed to ansure the query.
 
     you also need to return follow up questions to the user based on the question they have asked.
     The response needs to be structured like this -
-    {
-        followUps: [string],
-        answer: string
-    }
+    <ANSWER>
+    This is where the actual query should be answered
+    </ANSWER>
+
+    <FOLLOW_UPS>
+        <questions> first follow up question </questions>
+        <questions> second follow up question </questions>
+        <questions> third follow up question </questions>
+    </FOLLOW_UPS>
+
+    Example -
+    Query - I want to learn rust, can you suggest me the best ways to do it.
+    Response -
+    
+    <ANSWER>
+    For sure, the best resources to learn rust is the rust book
+    </ANSWER>
+
+    <FOLLOW_UPS>
+        <question> How can i learn advanced rust </question>
+        <question> How is rust better than typescript </question>
+    </FOLLOW_UPS>
 
 `
 export const PROMPT_TEMPLATE = `
